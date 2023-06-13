ecb93c06f4b3565b911674dca9c16886c313c5e9
Andry J. Hernandez <andryj9622@gmail.com>
2023-06-13 17:07:30 -0300

    feat: Define favorite routes

--- src/routes/v1/catalog/catalog.controller.ts
+++ src/routes/v1/catalog/catalog.controller.ts
@@ -1,16 +1,32 @@
 import express from 'express';
-import * as moviesService from './catalog.service';
+import * as catalogService from './catalog.service';
 
 const router = express.Router();
 
 router.get('/movies', (_req, res) => {
-  const movies = moviesService.getMovies();
+  const movies = catalogService.getMovies();
   res.json(movies);
 });
 
 router.get('/series', (_req, res) => {
-  const series = moviesService.getSeries();
+  const series = catalogService.getSeries();
   res.json(series);
+});
+
+router.get('/favorites/user/:userId', async (_req, res) => {
+  res.json('get favorites');
+});
+
+router.post('/favorites/user/', async (_req, res) => {
+  res.json('Add a favorite');
+});
+
+router.delete('/favorites/user/:userId/:contentId', async (_req, res) => {
+  res.json('remove a favorite');
+});
+
+router.get('/favorites/top', async (_req, res) => {
+  res.json('most favorite content');
 });
 
 export default router;
